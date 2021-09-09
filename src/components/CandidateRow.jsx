60c34f745815ddd2da736b317bab4df7f71f30ac
Eduardo <el_alienx@hotmail.com>
2021-09-09 15:39:47 +0200

    fix details

--- src/components/CandidateRow.jsx
+++ src/components/CandidateRow.jsx
@@ -1,12 +1,16 @@
 export default function CandidateRow({ item }) {
-  const { curriculumURL, name, portfolioURL, thumbnailURL } = item;
+  const { curriculumURL, name, portoflioURL, thumbnailURL } = item;
 
   return (
     <tr>
       <td>{thumbnailURL}</td>
       <td>{name}</td>
-      <td>{curriculumURL}</td>
-      <td>{portfolioURL}</td>
+      <td>
+        <a href={curriculumURL}>Curriculumn</a>
+      </td>
+      <td>
+        <a href={portoflioURL}>Portoflio</a>
+      </td>
     </tr>
   );
 }
