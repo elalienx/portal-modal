01348d95a02ef01bbbfeea12eed65d546d2b4c1e
Eduardo <el_alienx@hotmail.com>
2021-09-13 10:08:26 +0200

    fix the rendering location

--- src/components/ModalContainer.jsx
+++ src/components/ModalContainer.jsx
@@ -1,14 +1,17 @@
+import ReactDOM from "react-dom";
+
 export default function ModalContainer({ modal, setModal }) {
   // safeguard
   if (modal === null) return null;
 
-  return (
+  return ReactDOM.createPortal(
     <>
       <div className="modal-background"></div>
       <div className="modal-container">
         <button onClick={() => setModal(null)}>X</button>
         {modal}
       </div>
-    </>
+    </>,
+    document.getElementById("portal")
   );
 }
