c661b36cd371250472a2fe56ce46f87d8cd25317
Eduardo <el_alienx@hotmail.com>
2021-09-13 11:01:46 +0200

    added css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,19 @@
 import ReactDOM from "react-dom";
 
 export default function Modal({ state }) {
-  const { modal, setModal } = state;
+  const [modal, setModal] = state;
 
   // safeguard
   if (modal === null) return null;
 
   return ReactDOM.createPortal(
     <>
-      <div className="modal-background"></div>
-      <div className="modal-window">
+      <div className="background"></div>
+      <div className="window">
         <button onClick={() => setModal(null)}>X</button>
         {modal}
       </div>
     </>,
-    document.getElementById("portal")
+    document.getElementById("modal")
   );
 }
